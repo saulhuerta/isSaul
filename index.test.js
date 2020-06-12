1f5118879ed27d1b594480edfc33b4f9d0ec2f5a
Saul Huerta <saul.huerta@unosquare.com>
2020-06-11 19:28:51 -0500

    Add test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+var isSaul = require('./index')
+
+describe('Testing isSaul', function(){
+    it('should return true passing Geo', function(){
+        expect(isSaul('Geo')).toBeTruthy();
+    });
+ 
+    it('should return false passing Google', function(){
+        expect(isSaul('Goolge')).toBeFalsy();
+    });
+});
